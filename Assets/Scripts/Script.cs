7d39927bc09cae9eaa8935202a46d402031efb37
Daniel Caban <cabandaniel11@gmail.com>
2023-03-12 19:13:29 +0100

    skok

--- Assets/Scripts/Script.cs
+++ Assets/Scripts/Script.cs
@@ -1,18 +1,14 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Script : MonoBehaviour
 {
     // Start is called before the first frame update
-    void Start()
-    {
-        
+    void Start() {
+        // zmeny v kodu
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        
+    void Update() {
+
     }
 }

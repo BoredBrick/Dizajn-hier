1e2e42b9de9d5eb5fc4cfc1338792531c973f95d
Daniel Caban <cabandaniel11@gmail.com>
2023-04-04 21:48:42 +0200

    refactor

--- Assets/Scripts/Platform/PlatformColor.cs
+++ Assets/Scripts/Platform/PlatformColor.cs
@@ -5,27 +5,22 @@ public class PlatformColor : MonoBehaviour
     [SerializeField] GameObject player;
     [SerializeField] Color platformColor;
     private new SpriteRenderer renderer;
-    //private new Collider2D collider;
 
     private void Start()
     {
         renderer = GetComponent<SpriteRenderer>();
         renderer.color = Colors.GetRandomColor();
         platformColor = gameObject.GetComponent<SpriteRenderer>().color;
-        //collider = gameObject.GetComponent<Collider2D>();
-    }
-
-    private void Update()
-    {
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
+        Collider2D collider = collision.gameObject.GetComponent<Collider2D>();
         if (PlayerProperties.playerColor.Equals(platformColor))
-            collision.gameObject.GetComponent<Collider2D>().isTrigger = false;
+            collider.isTrigger = false;
         else
         {
-            collision.gameObject.GetComponent<Collider2D>().isTrigger = true;
+            collider.isTrigger = true;
             //player.transform.SetParent(null); TODO PRECO???
         }
     }

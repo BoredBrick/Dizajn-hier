f44be1da1dc27ac149e58e916bcb2499f236e0c4
Matúš <balazi@stud.uniza.sk>
2023-05-10 09:39:16 +0200

    Removed debugs and unused variables

--- Assets/Scripts/Platform/PlatformColor.cs
+++ Assets/Scripts/Platform/PlatformColor.cs
@@ -4,9 +4,7 @@ using UnityEngine;
 public class PlatformColor : MonoBehaviour
 {
     [SerializeField] private GameObject player;
-    [SerializeField] private Color defaultColor;
     [SerializeField] private Color platformColor;
-    private Color CurrentPlayerColor { get; set; }
     private new SpriteRenderer renderer;
     private new Collider2D collider;
 
@@ -14,15 +12,12 @@ public class PlatformColor : MonoBehaviour
     {
         renderer = GetComponent<SpriteRenderer>();
         renderer.color = Colors.GetRandomColor();
-        defaultColor = player.GetComponent<SpriteRenderer>().color;
         platformColor = gameObject.GetComponent<SpriteRenderer>().color;
-        CurrentPlayerColor = PlayerProperties.playerColor;
         collider = GetComponent<Collider2D>();
     }
 
     private void Update()
     {
-        Debug.Log(PlayerProperties.displayedColor);
         if ((PlayerProperties.playerColor == PlayerProperties.displayedColor)
             &&  PlayerProperties.playerColor == platformColor)
         {
@@ -36,35 +31,11 @@ public class PlatformColor : MonoBehaviour
             return;
         }
 
-        //zacina blednutie
         if (PlayerProperties.playerColor != PlayerProperties.displayedColor)
         {
             collider.enabled = true;
 
         }
-
-        /*
-        if (CurrentPlayerColor != PlayerProperties.playerColor)
-        {
-            CurrentPlayerColor = PlayerProperties.playerColor;
-
-            if (PlayerProperties.playerColor.Equals(platformColor)
-                && !PlayerProperties.playerColor.Equals(defaultColor))
-            {
-                //StartCoroutine(WaitForX(10f));
-                collider.enabled = true;
-            }
-            else
-            {
-                collider.enabled = false;
-            }
-        }
-        */
-    }
-
-    IEnumerator WaitForX(float waitTime)
-    {
-        yield return new WaitForSeconds(waitTime);       
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
